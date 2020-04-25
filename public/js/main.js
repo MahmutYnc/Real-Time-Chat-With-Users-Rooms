809805e223bc0c3532df8243560d6c6470df63fd
Mahmut <mahmutla33@gmail.com>
2020-04-25 23:41:13 +0300

    Bir şeyler hatalı

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room--name");
+const userList = document.getElementById("users");
+
+//Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+//Join Chat room
+socket.emit("joinRoom", { username, room });
+
+//Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room), outputUsers(users);
+});
+
+// Message from server
+socket.on("message", (message) => {
+  outputMessage(message);
+  console.log(message);
+
+  //Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //Get message text
+  const msg = e.target.elements.msg.value;
+
+  //Emit message to server
+  socket.emit("chatMessage", msg);
+
+  //Clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+//Output message to DOM
+function outputMessage(msg) {
+  const div = document.createElement("div");
+  div.classList.add("msg");
+  div.innerHTML = ` <p class= 'meta'> ${msg.username} <span> ${msg.time} </span></p>
+  <p class="text">
+     ${msg.text}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
+
+//Add room name to dom
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+//Add users to dom
+function outputUsers(users) {
+  userList.innerHTML = `
+  ${users.map((user) => `<li> ${user.username} </li >`).join("")}`;
+}
--- server.js
+++ server.js
@@ -1,12 +1,75 @@
-const path = require('path');
-const express = require('express');
-
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userLeave,
+  getRoomUsers,
+  userJoin,
+  getCurrentUser,
+} = require("./utils/users");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "Real Chat-Bot";
+
+//Run when a client connects
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    //Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+
+    //Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the channel`)
+      );
+
+    //send user and room indfo
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  //Listen for chat message
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.emit("message", formatMessage(user.username, msg));
+    console.log(msg);
+  });
+
+  //disconnect
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+    }
+
+    //send user and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+});
 
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
